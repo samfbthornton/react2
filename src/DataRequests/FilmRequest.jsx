eeae792c893aab57eb0779f206ef423761b5c95b
samfbthornton <samfbthornton@gmail.com>
2021-09-15 12:00:55 +0100

    added styling

--- src/DataRequests/FilmRequest.jsx
+++ src/DataRequests/FilmRequest.jsx
@@ -6,34 +6,49 @@ const FilmRequest = () => {
     const [filmTitle, setFilmTitle] = useState("");
     const apiKey = "140a3585";
 
-const getTitle = (e) => {
-    e.preventDefault(); 
-    setFilmTitle(e.target.value);
-}
+    const getTitle = (e) => {
+        e.preventDefault();
+        setFilmTitle(e.target.value);
+    }
 
-const sendRequest = (e) => {
-    axios.get("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + filmTitle)
-    .then(response => {
-        console.log(response.data);
-        setData(response.data);
-    });
-};
+    const sendRequest = (e) => {
+        axios.get("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + filmTitle)
+            .then(response => {
+                console.log(response.data);
+                setData(response.data);
+            });
+    };
 
-return (
-    <>
-    <h3>Film Request</h3>
-    <input type="text" 
-        onChange={(e)=>getTitle(e)}
-        placeholder="Enter Film Title"/>
-    <button onClick={(e) =>sendRequest(e)}>Click here!</button>
-    <br/>
-    <h4>{data.Title}</h4>
-    <h4>{data.Year}</h4>
-    <h4>{data.Plot}</h4>
-    <h4>{data.Runtime}</h4>
-    <img src={data.Poster} alt="Poster"></img>
-    </>
-);
+    return (
+        <>
+            <div style={{
+                backgroundColor: "lightblue",
+                borderStyle: "inset",
+                borderWidth: "thick"
+            }}>
+                <h3>Film Request</h3>
+                <input type="text"
+                    onChange={(e) => getTitle(e)}
+                    style={{
+                        float: "left",
+                        backgroundColor: "slateblue",
+                        color: "hotpink"
+                    }}
+                    placeholder="Enter Film Title" />
+                <button onClick={(e) => sendRequest(e)}
+                    style={{
+                        color: "hotpink",
+                        float: "left" 
+                        }}>Click here!</button>
+                <br />
+                <h4>{data.Title}</h4>
+                <h4>{data.Year}</h4>
+                <h4>{data.Plot}</h4>
+                <h4>{data.Runtime}</h4>
+                <img src={data.Poster} alt="Poster"></img>
+            </div>
+        </>
+    );
 }
 
 export default FilmRequest
